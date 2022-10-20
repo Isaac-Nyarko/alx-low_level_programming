741d10d51d8df8f33322a9d2f2320b7daaa00e33
Isaac-Nyarko <isaacofosu935@gmail.com>
2022-10-20 05:18:11 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+void print_triangle(int size)
+
+{
+
+		int row, hashes, spaces;
+
+
+
+			if (size <= 0)
+
+					{
+
+								_putchar('\n');
+
+									}
+
+				else
+
+						{
+
+									for (row = 1; row <= size; row++)
+
+												{
+
+																for (spaces = size - r
