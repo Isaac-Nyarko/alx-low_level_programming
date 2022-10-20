5df686ce62b9629d9d683364b6e56fda24a47a0a
Isaac-Nyarko <isaacofosu935@gmail.com>
2022-10-20 04:49:51 -0700

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+nclude "main.h"
+
+
+
+/**
+ *
+ * *print_most_numbers - prints
+ *
+ * * owned by Bwave ICT
+ *
+ * *Return: void
+ *
+ * */
+
+
+
+void print_most_numbers(void)
+
+{
+
+		char c;
+
+
